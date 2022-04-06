b78df8dba523c305ef13ba35e9af797e03c7a81b
Yusufboy Atajanov <Yusufboy_Atajanov@epam.com>
2022-04-06 13:15:34 +0500

    Project initialized

---
+++ TestProject1/DeleteMe.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace TestProject1
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void Test1()=>
+            Assert.True(true);
+        
+    }
+}
